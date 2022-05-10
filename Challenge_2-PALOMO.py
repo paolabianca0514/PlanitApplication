d909f973c081c2f8df778f8683360602a1d1bcaf
paolabianca0514 <104632738+paolabianca0514@users.noreply.github.com>
2022-05-10 12:47:43 +1000

    Update Challenge_2-PALOMO.py

--- Challenge_2-PALOMO.py
+++ Challenge_2-PALOMO.py
@@ -4,7 +4,7 @@ If there is more than one character with equal highest occurrences,
 return the character that appeared first  within the string.'''
 while True:
     from collections import Counter#dictionary sublass for counting hashable items
-    user_input = input('Please enter a string:').lower().split()#accepts user input and converts it to lowercase letters
+    user_input = input('Please enter a string:').lower().strip()#accepts user input and converts it to lowercase letters
     count = Counter(user_input)#creates an instance for the input, counts occurence for each letter
     count = max(count, key = count.get)#obtains the letter with the maximum occurence through get function
     print('The character with the highest number of occurence from the string \'{}\' is \'{}\'.'
